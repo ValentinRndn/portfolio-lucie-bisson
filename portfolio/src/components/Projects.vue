79ec103d06bf300c2483bc4cb45beeacfbe65845
Valentin <valentinrenaudin2206@gmail.com>
2024-11-24 23:20:25 +0100

    Remove unused PDF assets and update project structure in Projects component

--- portfolio/src/components/Projects.vue
+++ portfolio/src/components/Projects.vue
@@ -76,16 +76,68 @@
         </div>
     </div>
 
-    <div class="academic-projects-content flex justify-center  gap-8 mx-8 mt-8">
-        <img src="../assets/projects/charrues/pancart.png" 
-             alt="pancart-charrues" 
-             class="w-[48%] flex-shrink-0">
-        <img src="../assets/projects/pettokaskes/mockup-flyer.png" 
-             alt="mockup-flyer" 
-             class="w-[48%] flex-shrink-0">
-    </div>
+    <div class="projects-grid-container grid grid-cols-2 gap-8 p-8">
+    <img src="../assets/projects/charrues/pancart.png" alt="pancart-charrues" class="w-full h-auto">
+    <img src="../assets/projects/pettokaskes/mockup-flyer.png" alt="mockup-flyer" class="w-full h-auto">
+    <img src="../assets/projects/purina/logo.png" alt="logo-purina" class="w-full h-auto bg-white">
+    <img src="../assets/projects/reviers/mockup.png" alt="mockup-reviers" class="w-full h-auto">
+</div>
 </div>
 
+<div class="professional-projects-container w-full mt-24">
+    <div class="top-professional-projetcs-container flex justify-center items-center flex-row-reverse">
+        <div class="left-professional-projects-container-title w-3/5 flex items-center justify-center">
+            <span class="text-[8rem] relative font-bold text-darkred md:text-[5rem]">
+                Professionels
+                <span class="text-lightpink font-relation text-[6rem] absolute left-10 top-10">
+                    Projets
+                </span>
+            </span>
+        </div>
+        <div class="left-professional-projects-container-title flex flex-col w-2/5 overflow-hidden bg-lightred">
+            <span id="academic" class="text-darkred text-[15rem] font-bold">SSIONNEL</span>
+            <span id="academic" class="text-darkred text-[15rem] font-bold ml-28 -mt-40">PROFESSIONNEL</span>
+        </div>
+    </div>
+
+    <div class="projects-grid-container grid grid-cols-2 gap-8 p-8">
+    <img src="../assets/projects/bienvoir/magazines.png" alt="bienvoir-magazine" class="w-full h-auto">
+    <img src="../assets/projects/bienvoir/article_1.png" alt="bienvoir-article-1" class="w-full h-auto ">
+    <img src="../assets/projects/bienvoir/article_2.png" alt="bienvoir-article-2" class="w-full h-auto -mt-[470px]">
+    <img src="../assets/projects/bienvoir/article_3.png" alt="bienvoir-article-3" class="w-full h-auto">
+    <img src="../assets/projects/bienvoir/journal.png" alt="bienvoir-journal" class="w-full h-auto -mt-[470px]">
+
+</div>
+</div>
+
+
+
+<div class="personnal-projects-container w-full mt-24">
+    <div class="top-personnal-projetcs-container flex justify-center items-center">
+        <div class="left-personnal-projects-container-title w-3/5 flex items-center justify-center">
+            <span class="text-[8rem] relative font-bold text-darkred md:text-[5rem]">
+                Personnels
+                <span class="text-lightpink font-relation text-[6rem] absolute left-10 top-10">
+                    Projets
+                </span>
+            </span>
+        </div>
+        <div class="left-personnal-projects-container-title flex flex-col w-2/5 overflow-hidden bg-lightred">
+            <span id="academic" class="text-darkred text-[15rem] font-bold">ONNEL</span>
+            <span id="academic" class="text-darkred text-[15rem] font-bold ml-28 -mt-40">PERSONNEL</span>
+        </div>
+    </div>
+
+    <div class="projects-grid-container grid grid-cols-2 gap-8 p-8">
+    <img src="../assets/projects/lucie/carte.png" alt="carte-lucie" class="w-full h-auto">
+    <!-- <img src="../assets/projects/lucie/charte.png" alt="charte-lucie" class="w-full h-auto"> -->
+    <img src="../assets/projects/lucie/todbag.png" alt="todbag-lucie" class="w-full h-auto">
+    <img src="../assets/projects/lucie/logostacklab.png" alt="logo-stacklab" class="w-full h-auto">
+
+
+
+</div>
+</div>
 
 </div>
   </template>
