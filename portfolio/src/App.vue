55f6143502c4cff1c4a86373d4d491ff5e146dee
Valentin <valentinrenaudin2206@gmail.com>
2024-11-25 19:31:27 +0100

    [FIX] Curriculum content

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -16,6 +16,5 @@
 <script setup> 
 import Navbar from './components/Navbar.vue'
 import Welcome from './components/Welcome.vue';
-  import Footer from './components/Footer.vue';
   import Socials from './components/Socials.vue';
 </script>
--- portfolio/src/components/Curriculum.vue
+++ portfolio/src/components/Curriculum.vue
@@ -101,7 +101,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 16 16"><path fill="#F9B29C" fill-rule="evenodd" d="M6 1a1.75 1.75 0 0 0-1.75 1.75V4H3a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.25V2.75A1.75 1.75 0 0 0 10 1zm4.25 3V2.75A.25.25 0 0 0 10 2.5H6a.25.25 0 0 0-.25.25V4zM3 5.5h10a.5.5 0 0 1 .5.5v1h-11V6a.5.5 0 0 1 .5-.5m-.5 3V13a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8.5H9V10H7V8.5z" clip-rule="evenodd"/></svg>
     </div>
     <div class="right-list-container flex flex-col justify-center text-d">
-      <span class="date text-[1.1rem]">Sep 2023 - Aou 2024 </span>
+      <span class="date text-[1.1rem]">Sep 2023 - Aou 2025 </span>
       <span class=""><span class="">BTS</span> <span class="font-bold text-[1.5rem]">- COMMUNICATION</span></span>
       <span class="text-[1.1rem]">Aftec</span>
     </div>
@@ -112,7 +112,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 16 16"><path fill="#F9B29C" fill-rule="evenodd" d="M6 1a1.75 1.75 0 0 0-1.75 1.75V4H3a2 2 0 0 0-2 2v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.25V2.75A1.75 1.75 0 0 0 10 1zm4.25 3V2.75A.25.25 0 0 0 10 2.5H6a.25.25 0 0 0-.25.25V4zM3 5.5h10a.5.5 0 0 1 .5.5v1h-11V6a.5.5 0 0 1 .5-.5m-.5 3V13a.5.5 0 0 0 .5.5h10a.5.5 0 0 0 .5-.5V8.5H9V10H7V8.5z" clip-rule="evenodd"/></svg>
     </div>
     <div class="right-list-container flex flex-col justify-center text-d">
-      <span class="date text-[1.1rem]">Sep 2018 - Aou 2021 </span>
+      <span class="date text-[1.1rem]">Sep 2020 - Jui 2022 </span>
       <span class=""><span class="">Lycée</span> <span class="font-bold text-[1.5rem]">- STMG</span></span>
       <span class="text-[1.1rem]">Arcisse de Caumont</span>
     </div>
